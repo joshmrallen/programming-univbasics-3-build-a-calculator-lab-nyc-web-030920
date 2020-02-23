79de0de3ab42eee41a976c28c7c55aa3751dc1dd
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-23 02:05:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,24 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  
+  num1 - num2
 end
 
-def division(num1, num2)
-
+def division(num1, num2 = 1)
+  if (num2 != 0)
+    num1/num2
+  else 
+    puts "Cannot divde by zero."
 end
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
